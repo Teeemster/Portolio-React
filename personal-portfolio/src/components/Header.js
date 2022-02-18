47b7a38164716a3f2627d866149bf2813274b68c
Teeemster <jonathan_beach@ymail.com>
2022-02-17 21:45:06 -0700

    added header

---
+++ personal-portfolio/src/components/Header.js
@@ -0,0 +1,14 @@
+//Header Component
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+    return (
+        <div>
+          About Me | Portfolio | Contact | Resume
+        </div>
+    );
+};
+
+export default Header;
